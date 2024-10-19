578fd7ece6f3cfb3574fba52573e6cd7badbc749
Ixhika <106822722+Ixhika@users.noreply.github.com>
2024-10-19 10:22:11 +0530

    up

---
+++ C++/Basics.cpp
@@ -0,0 +1,18 @@
+/*To take input to produce output we use the library iostream
+For mathematical functional we use math.h*/
+
+/*if we use namespace std library we do not have to use std again and again...it autmatically uses std for every function*/
+
+/*std -  is the one under which cout comes
+endl - is used to print something on next line*/
+
+/*#include<bits/stdc++.h> : includes every single library required for program. and includes all data types too*/
+
+#include<iostream>
+using namespace std;
+int main(){
+    int x ,y;
+    cin>>x >> y;   //way of taking single input or multiple input
+    cout<<"Value of x"<<x << y;
+    return 0;
+}
---
+++ C++/t01.cpp
@@ -0,0 +1,48 @@
+//this is header file
+#include <iostream>  //this lib used for taking input and give output to console
+using namespace std; //std is dictionary for vector cout cin...works as keywords
+
+// :: this is scope resolution operator
+int main(){
+
+    int a;
+    cin>>a;
+    cout<<a;
+    return 0;
+}
+
+// arr of heap allocated and stack allocated
+// delete:  does not wastes any memory
+// sizeof(anything) gives byte size eg a= 12 sizeof(a) =  4
+// long : memory in min equal to INT . is dynamic , makes according to size
+// long long : it creates the size fully and then starts operation(static)
+
+// signed: deafult(31 bits)
+// unsigned: 0 to 20 range....but we can write pow(2,32)
+
+
+
+/*call by reference abd pass by value*/
+
+int a = 10;
+int main(){
+    int a = 90;
+    cout<<"Local Variable----"<<a<<endl;
+    cout<<"global variable----"<<::a<<endl;
+}
+
+
+// call by reference
+int main(){
+    int a = 10;
+    int& b = a;   //&:empercent symbol used to refer the variable a
+    return 0;
+}
+
+// const in c++ keyword works as static keyword in java
+
+// * : deference of operator
+
+int a = 3;
+int* b = &a; //b is just a ptr which stores the address of a(not the value of a)
+// &a means address of a
